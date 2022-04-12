2309d95ad34cfe3692527d8d0ecfb7e89b352d4a
buherator <buherator@silentsignal.hu>
2022-04-12 16:41:23 +0200

    cat

---
+++ org/apache/commons/Collections.java
@@ -0,0 +1,38 @@
+/*
+ ct                                                      
+pink          ct                                         
+pink!        big                                         
+rather big solid                                         
+wild, epic, fine                                         
+ ever so hungry                                          
+ ever so hungry                                          
+positively pink                                          
+extremely hungry                                         
+wild, epic, fine                                         
+ positively pink                                         
+ decidedly pretty                                        
+ mad men mooing, funnily                                 
+ wild hands lowing, funnily                              
+ demented men lowing, funnily                            
+ mad work force mooing, funnily                          
+ insane manpower lowing, funnily                         
+ excited manpower lowing, funnily                        
+  demented custody lowing, funnily                       
+  its angry custody lowing, funnily                      
+   mad men, hard bole, active twigs                      
+    demented custody lowing, funnily                     
+    distracted hands mooing, funnily                     
+     plain trunk huffing, graciously                     
+     plain body snorting, graciously                     
+     insane workforce lowing, funnily                    
+      its frantic men mooing, funnily                    
+      sound torso huffing, graciously                    
+      solid trunk huffing, graciously                    
+      ever so mad men lowing, funnily                    
+     excited custody, noble tree trunk, active twigs     
+   angry hands, plain luggage compartment, active twigs  
+  quite solid crazy hands, strong body, hyperactive twigs
+     upchuck  insane hands, solid bole, hyperactive twigs
+      solid                                      so solid
+                                                 mooing  
+*/
